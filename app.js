52f95b57aa370eeac52bd6067aa46f4a16da3eb6
Henrique Portela <henriquepsoares@gmail.com>
2023-03-16 17:12:05 -0300

    Update app.js / agentRouter

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ import cors from 'cors'
 import connectDb from './config/db.connection.js'
 import authRouter from './routes/auth.routes.js'
 import houseRouter from './routes/house.routes.js'
+import agentRouter from './routes/realEstateAgent.routes.js'
 
 
 const app = express()
@@ -14,6 +15,7 @@ app.use(cors())
 app.use(express.json())
 app.use(authRouter)
 app.use(houseRouter)
+app.use(agentRouter)
 
 
 
